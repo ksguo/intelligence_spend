a838fb2b32f6422e5bb715acbb13b2b6043f4af8
ks_guo <kguo1@smail.uni-koeln.de>
2025-04-14 22:39:33 +0200

    add env-prodution

--- app/core/db.py
+++ app/core/db.py
@@ -3,26 +3,17 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
+# Add SSL parameters for production environments
+import os
+
 # SQLAlchemy database URL
 SQLALCHEMY_DATABASE_URL = (
     f"postgresql://{settings.database_username}:{settings.database_password}@"
     f"{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 )
 
-# Create the SQLAlchemy engine
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# Create a sessionmaker
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    except Exception as e:
-        db.rollback()
-        raise e  # Re-raise the exception so FastAPI can handle it
-    finally:
-        db.close()
+# Create the SQLAlchemy engine with SSL for production
+if os.environ.get("ENVIRONMENT") == "production":
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "require"})
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
